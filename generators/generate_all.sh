661e054f0cf0edbb88f98e4474e5f3366c0fcd99
Sven Rosvall <sven@rosvall.ie>
2024-10-16 23:48:41 +0100

    CANMIO: Improve option parsing.

--- generators/generate_all.sh
+++ generators/generate_all.sh
@@ -11,20 +11,20 @@ echo "Generated $wip_dir/CANLEVER-0D20-1a.json"
 $gen_dir/generate_CANMIO-SVO.sh > $wip_dir/CANMIO-SVO-A532-4s.json
 echo "Generated $wip_dir/CANMIO-SVO-A532-4s.json"
 
-$gen_dir/generate_CANMIO.sh 3a > $wip_dir/CANMIO-A520-3a.json
+$gen_dir/generate_CANMIO.sh -v 3a > $wip_dir/CANMIO-A520-3a.json
 echo "Generated $wip_dir/CANMIO-A520-3a.json"
-$gen_dir/generate_CANMIO.sh 3c > $wip_dir/CANMIO-A520-3c.json
+$gen_dir/generate_CANMIO.sh -v 3c > $wip_dir/CANMIO-A520-3c.json
 echo "Generated $wip_dir/CANMIO-A520-3c.json"
-$gen_dir/generate_CANMIO.sh 3d > $wip_dir/CANMIO-A520-3d.json
+$gen_dir/generate_CANMIO.sh -v 3d > $wip_dir/CANMIO-A520-3d.json
 echo "Generated $wip_dir/CANMIO-A520-3d.json"
-$gen_dir/generate_CANMIO.sh 3e > $wip_dir/CANMIO-A520-3e.json
+$gen_dir/generate_CANMIO.sh -v 3e > $wip_dir/CANMIO-A520-3e.json
 echo "Generated $wip_dir/CANMIO-A520-3e.json"
-$gen_dir/generate_CANMIO.sh 4a > $wip_dir/CANMIO-A520-4a.json
+$gen_dir/generate_CANMIO.sh -v 4a > $wip_dir/CANMIO-A520-4a.json
 echo "Generated $wip_dir/CANMIO-A520-4a.json"
 
-$gen_dir/generate_CANMIO.sh XIO 3e > $wip_dir/CANXIO-A540-3e.json
+$gen_dir/generate_CANMIO.sh -t XIO -v 3e > $wip_dir/CANXIO-A540-3e.json
 echo "Generated $wip_dir/CANXIO-A540-3e.json"
-$gen_dir/generate_CANMIO.sh XIO 4a > $wip_dir/CANXIO-A540-4a.json
+$gen_dir/generate_CANMIO.sh -t XIO -v 4a > $wip_dir/CANXIO-A540-4a.json
 echo "Generated $wip_dir/CANXIO-A540-4a.json"
 
 $gen_dir/generate_CANPAN.sh > $wip_dir/CANPAN-A51D-1y.json
